5ebd683e1c9eecddfd80c90136020374fea3cee7
Gustavo L. Carvalho <gustavocrvlh.02@gmail.com>
2025-01-30 16:28:36 -0300

    scores

---
+++ week2/scores.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+const int N = 3;
+
+float average(int length, int array[]);
+
+int main(void){
+    
+    int scores[N];
+    for (int i = 0; i < N; i++){
+        printf("Enter score %d: ", i);
+        scanf("%i", &scores[i]);
+    }
+
+
+    printf("Average: %.2f\n", average(N, scores));
+}
+
+float average(int length, int array[]){
+    int sum = 0;
+    for (int i = 0; i < length; i++){
+        sum += array[i];
+    }
+    return sum / length;
+}
